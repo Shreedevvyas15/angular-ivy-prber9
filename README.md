814007e20ebb496bf9f0139c49d77e15e008dc9e
Shreedevvyas15 <dvsachin15@gmail.com>
2022-04-14 13:31:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-prber9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-prber9)
---
+++ src/app/name/name.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+     selector:'my-name',
+     templateUrl:'./name.component.html',
+     styleUrls:['./name.component.css']
+})
+export class NameComponent {
+
+}
